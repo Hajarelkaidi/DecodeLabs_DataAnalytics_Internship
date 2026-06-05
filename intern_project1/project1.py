2ec3182cf2b60f7b7c170f48f15017bfae74d868
HAJAR ELKAIDI <hajar.elkaidi00@gmail.com>
2026-06-05 02:32:33 +0100

    some changement

--- intern_project1/project1.py
+++ intern_project1/project1.py
@@ -1,7 +1,10 @@
 import pandas as pd 
 import numpy as np
 
+# ************************************
 # ******** Data Understanding ********
+# ************************************
+
 # ====== step 1: Load dataset ======
 data = pd.read_excel("Dataset for Data Analytics.xlsx")
 
@@ -10,13 +13,30 @@ print(data.head())
 print(data.describe())
 data.info()
 
-# ====== step 3: Missing Values ======
+# ====== step 3: Missing Analysis ======
         # Number of missing values: 
 missing_values = data.isnull().sum()
-print("The numbers of missing values are:\n",missing_values)
+print("Missing values before cleaning are:\n",missing_values)
         # Percentage of missing values:
 per_missing_values = data.isnull().mean() * 100
 print("\nThe percentages of the missing values are:\n",per_missing_values)
+        # I observe that Only "CouponCode" column contains missing values,
+        # 309 missing values (25.75% of the dataset),
+        # and all other columns have complete data.
 
+# ====== step 4: Duplicate Analysis ======
+        # Search duplicate rows:
+print("Duplicate rows:", data.duplicated().sum())
+        # Search duplicate Order IDs:
+print("Duplicate Order IDs:", data["OrderID"].duplicated().sum())
+
+# *******************************
 # ******** Data Cleaning ********
-# ====== step 4: Handling Missing Values ======
+# *******************************
+
+# ====== step 5: Handling Missing Values ======
+        # Fill missing CouponCode values:
+data["CouponCode"] = data["CouponCode"].fillna("Not Used")
+        # Verify missing values:
+print("Missing values after cleaning are:\n",data.isnull().sum())
+
